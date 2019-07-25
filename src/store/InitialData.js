726c26753a60c2a34d7eef4b10cb4bf0a230a353
Ehimah Obuse <ehimahobuse@gmail.com>
2019-07-25 23:08:53 +0100

    adding react store

---
+++ src/store/InitialData.js
@@ -0,0 +1,46 @@
+import LINKS from './Types';
+
+export const data = {
+  [LINKS]: [
+    {
+      title: 'McKinsey Advised Johnson and Johnson on Increasing Opioid Sales',
+      url:
+        'https://www.nytimes.com/2019/07/25/business/mckinsey-johnson-and-johnson-opioids.html',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title:
+        'Chris Hughers, a Facebook Founder, Is Working with the Government to Break It Up',
+      url:
+        'https://www.nytimes.com/2019/07/25/technology/chris-hughes-facebook-breakup.html',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title: 'MQTT: A Conceptual Deep-Dive',
+      url: 'https://www.ably.io/concepts/mqtt',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title: 'ChaiScript – Easy to Use Scripting for C++',
+      url: 'http://chaiscript.com/',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title: "Brain Simulation Promised a Decade Ago Hasn't Succeeded",
+      url:
+        'https://www.theatlantic.com/science/archive/2019/07/ten-years-human-brain-project-simulation-markram-ted-talk/594493/',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title: 'Tulsi Gabbard Sues Google for Suspending Ad Account',
+      url:
+        'https://www.nytimes.com/2019/07/25/technology/tulsi-gabbard-sues-google.html',
+      __typename: 'HackerNewsItem'
+    },
+    {
+      title: 'An Introduction to Recurrent Neural Networks',
+      url: 'https://victorzhou.com/blog/intro-to-rnns/',
+      __typename: 'HackerNewsItem'
+    }
+  ]
+};
